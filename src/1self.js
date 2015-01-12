fbc489a68be73acef0d9bcf15d5e1f25ca75b0cf
Douglas Vaz <douglas1992@gmail.com>
2015-01-12 18:42:54 +0530

    Add offline support

--- src/1self.js
+++ src/1self.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
     if (typeof define === "function" && define.amd) {
         define([], factory);
     } else if (typeof exports === "object") {
@@ -6,75 +6,84 @@
     } else {
         root.lib1self = factory();
     }
-}(this, function (context) {
+}(this, function(context) {
     'use strict';
 
     var API_ENDPOINT = "https://api-staging.1self.co";
 
-    var send = function (endpoint, data, headers, callback) {
-        var req = new XMLHttpRequest();
+    var loadConfig = function() {
+        return JSON.parse(window.localStorage.config);
+    };
 
-        req.open("POST", endpoint, true);
+    var loadJSON = function(key) {
+        try {
+            return JSON.parse(window.localStorage[key]);
+        } catch (e) {
+            return {};
+        }
+    };
 
-        if (typeof headers === "object") {
+    var queue = function() {
+        var stored = loadJSON('1self');
+        if (typeof stored.events === 'undefined') {
+            stored.events = [];
+        }
+        return stored;
+    }();
+
+    var saveJSON = function(obj, key) {
+        window.localStorage[key] = JSON.stringify(obj);
+    };
+
+    var queueEvent = function(event) {
+        queue.events.push(event);
+        saveJSON(queue, '1self');
+    };
+
+    var lock = false;
+    var poller = function() {
+        if (!lock) {
+            var config = loadConfig();
+            var event_api_endpoint = API_ENDPOINT + "/v1/streams/" + config.streamid + "/events/batch";
+
+            lock = true;
+
+            var req = new XMLHttpRequest();
+            req.open("POST", event_api_endpoint, true);
+
+            var headers = {
+                "Authorization": config.writeToken,
+                "Content-Type": "application/json"
+            };
             var keys = Object.keys(headers);
-            keys.forEach(function (key) {
+            keys.forEach(function(key) {
                 req.setRequestHeader(key, headers[key]);
             });
-        }
 
-        req.onload = function () {
-            if (req.readyState == 4 && req.status == 200) {
-                if (callback) {
-                    callback(req.responseText);
+            req.onload = function() {
+                if (req.readyState == 4 && req.status == 200) {
+                    queue.events = [];
+                    saveJSON(queue, '1self');
+                    lock = false;
+                } else {
+                    lock = false;
+                    console.log(new Error(req.statusText + "\n" + req.responseText));
                 }
-            } else {
-                throw (new Error(req.statusText));
+            };
+
+            req.onerror = function() {
+                console.log(new Error("Network Error"));
+                lock = false;
+            };
+
+            if(queue.events.length > 0){ 
+                req.send(JSON.stringify(queue.events));
             }
-        };
-        req.onerror = function () {
-            throw (Error("Network Error"));
-        };
-
-        req.send(JSON.stringify(data));
-    };
-
-    var syncService = function () {
-
-        //Load localstorage
-        var loadJSON = function(key) {
-            try{
-                return JSON.parse(window.localStorage[key]);
-            } catch(e) {
-                return {};
-            }
-        };
-
-        //save localstorage
-        var saveJSON = function(obj, key) {
-            window.localStorage[key] = JSON.stringify(obj);
+            setTimeout(poller, 2000);
         }
-
-        var SAVED_STATE = function(){
-            var stored = loadJSON('1self');
-            if(typeof stored.events === 'undefined') {
-                stored.events = [];
-            }
-            return stored;
-        }();
-
-        // logEvent
-        this.logEvents = function (event) {
-            SAVED_STATE.events.push(event);
-            saveJSON(SAVED_STATE);
-        }
-
-        // pollForAnyEvent
-        // yes -> sendEvents
-
     };
-
-    var lib1self = function (config) {
+    
+    var lib1self = function(config) {
         this.OBJECT_TAGS = [];
         this.ACTION_TAGS = [];
 
@@ -82,7 +91,7 @@
             window.localStorage.config = "{}";
         }
 
-        var saved_config = JSON.parse(window.localStorage.config);
+        var saved_config = loadConfig();
         if (typeof config === 'object') {
             var keys = Object.keys(config);
             for (var i = 0; i < keys.length; i++) {
@@ -93,18 +102,27 @@
 
         window.localStorage.config = JSON.stringify(saved_config);
         this.config = saved_config;
+
+        if(typeof this.config.streamid !== 'undefined') {
+            poller();
+        }
         return this;
     };
 
-    lib1self.prototype.saveConfig = function () {
+    lib1self.prototype.loadConfig = function() {
+        this.config = loadConfig();
+        return this.config;
+    };
+
+    lib1self.prototype.saveConfig = function() {
         window.localStorage.config = JSON.stringify(this.config);
         return this;
     }
 
-    lib1self.prototype.configure = function (data) {
+    lib1self.prototype.configure = function(data) {
         var self = this;
         if (typeof data !== 'undefined') {
-            Object.keys(data).forEach(function (key) {
+            Object.keys(data).forEach(function(key) {
                 self.config[key] = data[key];
             });
         }
@@ -112,7 +130,7 @@
         return this;
     };
 
-    lib1self.prototype.registerStream = function (callback) {
+    lib1self.prototype.registerStream = function(callback) {
         if (!this.config.appId || !this.config.appSecret) {
             throw new Error("Set appId and appSecret");
         }
@@ -123,28 +141,33 @@
 
         req.open("POST", API_ENDPOINT + "/v1/streams", true);
         req.setRequestHeader("Authorization", self.config.appId + ":" + self.config.appSecret);
-        req.onload = function () {
+        req.onload = function() {
             if (req.readyState == 4 && req.status == 200) {
                 var response = JSON.parse(req.response);
-                self.configure({'streamid': response.streamid});
-                self.configure({'readToken': response.readToken});
-                self.configure({'writeToken': response.writeToken});
+                self.configure({
+                    'streamid': response.streamid
+                });
+                self.configure({
+                    'readToken': response.readToken
+                });
+                self.configure({
+                    'writeToken': response.writeToken
+                });
 
                 callback(response);
             } else {
                 throw (new Error(req.statusText));
             }
         };
-        req.onerror = function () {
+        req.onerror = function() {
             throw (Error("Network Error"));
         };
         req.send();
         return this;
     };
 
-    lib1self.prototype.sendEvent = function (event, callback) {
 
-        var event_api_endpoint = API_ENDPOINT + "/v1/streams/" + this.config.streamid + "/events/";
+    lib1self.prototype.sendEvent = function(event, callback) {
 
         if (!event.dateTime) {
             event.dateTime = (new Date()).toISOString();
@@ -165,56 +188,60 @@
 
         var streamid = this.config.streamid;
         if (!streamid) {
-            this.registerStream(function (response) {
-                streamid = response.streamid;
-                send(event_api_endpoint, event, headers, callback);
+            var self = this;
+            this.registerStream(function(response) {
+                self.config.streamid = response.streamid;
+                queueEvent(event);
+                poller();
+                callback();
             })
         } else {
-            send(event_api_endpoint, event, headers, callback);
+            queueEvent(event);
+            callback();
         }
         return this;
     };
 
-    lib1self.prototype.objectTags = function (tags) {
+    lib1self.prototype.objectTags = function(tags) {
         this.OBJECT_TAGS = tags;
         return this;
     };
 
-    lib1self.prototype.actionTags = function (tags) {
+    lib1self.prototype.actionTags = function(tags) {
         this.ACTION_TAGS = tags;
         return this;
     };
 
-    lib1self.prototype.sum = function (property) {
+    lib1self.prototype.sum = function(property) {
         this.FUNCTION_TYPE = 'sum(' + property + ')';
         this.SELECTED_PROP = property;
         return this;
     };
 
-    lib1self.prototype.count = function () {
+    lib1self.prototype.count = function() {
         this.FUNCTION_TYPE = 'count';
         return this;
     };
 
-    lib1self.prototype.barChart = function () {
+    lib1self.prototype.barChart = function() {
         this.CHART_TYPE = 'barchart';
         return this;
     }
 
-    lib1self.prototype.json = function () {
+    lib1self.prototype.json = function() {
         this.CHART_TYPE = 'type/json';
         return this;
     };
 
-    lib1self.prototype.url = function () {
+    lib1self.prototype.url = function() {
         //Check
         if (this.OBJECT_TAGS.length == 0 || this.ACTION_TAGS.length == 0 || !this.config.streamid || !this.FUNCTION_TYPE || !this.CHART_TYPE) {
             throw (new Error("Can't construct URL"));
         }
 
-        var stringifyTags = function (tags) {
+        var stringifyTags = function(tags) {
             var str = "";
-            tags.forEach(function (tag) {
+            tags.forEach(function(tag) {
                 str += tag + ',';
             });
             return str.slice(0, -1);
